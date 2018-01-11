685e6fc794a99ed2821041136f20d595ae063855
GeoffreyFuller <gf0911757@RK03-B1225-14.francistuttle.edu>
2018-01-11 09:08:08 -0600

    First Program Iteration

--- src/Chapter5/C5_46.java
+++ src/Chapter5/C5_46.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Reverses a user-inputed string
+ * @author Geoffrey Fuller
  */
 package Chapter5;
-
+import java.util.Scanner;
 /**
- *
- * @author gf0911757
- */
+* Main Method
+*
+* @param args arguments from command line prompt
+*/
 public class C5_46 {
-    
+        public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a word or phrase:");
+        String s = input.nextLine();
+        String s2 = "";
+        for (int i = s.length() - 1; i >= 0; i--)
+        {
+            s2 += s.charAt(i);
+        }
+        System.out.println("The reversed string is " + s2);
+    }
 }
--- src/Chapter5/P5.java
+++ src/Chapter5/P5.java
@@ -1,14 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Program allows user to vote
+ * @author Geoffrey Fuller
  */
 package Chapter5;
-
+import java.util.Scanner;
 /**
- *
- * @author gf0911757
- */
+* Main Method
+*
+* @param args arguments from command line prompt
+*/
 public class P5 {
-    
+     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a Y for yes, an N for no, or a Q to quit voting: ");
+        String vote = input.nextLine().toUpperCase();
+
+        int countY = 0;
+        int countN = 0;
+        int countI = 0;
+        int total = 0;
+
+        while (!vote.equalsIgnoreCase("Q")) {
+            
+            switch (vote) {
+                case "Y":
+                    countY++;
+                    break;
+
+                case "N":
+                    countN++;
+                    break;
+
+                default:
+                    System.out.println("INVALID CODE: Enter 'Y' to vote yes, 'N' to vote no, or 'Q' to quit voting.");
+                    countI++;
+                    break;
+            }
+            System.out.println("Enter a Y for yes, an N for no, or a Q to quit voting: ");
+            vote = input.nextLine().toUpperCase();
+
+        }
+
+        System.out.println("Yes votes: " + countY);
+        System.out.println("No votes: " + countN);
+        System.out.println("Invalid votes: " + countI);
+        System.out.println("Total votes: " + (countY + countN + countI));
+        System.exit(0);
+    }
 }
