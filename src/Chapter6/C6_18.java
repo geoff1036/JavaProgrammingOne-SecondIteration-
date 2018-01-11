45801d3c8df6aa3ea7709e8369dd3203cfc57318
GeoffreyFuller <gf0911757@RK03-B1225-14.francistuttle.edu>
2018-01-11 09:08:21 -0600

    First Program Iteration

--- src/Chapter6/C6_18.java
+++ src/Chapter6/C6_18.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Program allows user to vote
+ * @author Geoffrey Fuller
  */
 package Chapter6;
-
+import java.util.Scanner;
 /**
- *
- * @author gf0911757
- */
+* Main Method
+*
+* @param args arguments from command line prompt
+*/
 public class C6_18 {
-    
+        public static void main (String[] args)
+    {
+        Scanner input = new Scanner(System.in);	
+		System.out.print("Enter a password: ");
+		String password = input.nextLine();
+		System.out.println(
+			(isValidPassword(password) ? "Valid " : "Invalid ") + "Password");
+	}
+	public static boolean isValidPassword(String password)
+        {
+		final int validLength = 8;	
+	   final int minDigits = 2;	
+		boolean validPassword = 
+			isLengthValid(password, validLength) && 
+			isOnlyLettersAndDigits(password) &&
+			hasNDigits(password, minDigits);
+
+		return validPassword;
+	}
+	public static boolean isLengthValid(String password, int validLength)
+        {
+		return password.length() >= validLength;
+	}
+	public static boolean isOnlyLettersAndDigits(String password)
+        {
+		for (int i = 0; i < password.length(); i++)
+                {
+			if (!Character.isLetterOrDigit(password.charAt(i)))
+                        {
+                        return false;
+			}
+		}
+		return true;
+	}
+	public static boolean hasNDigits(String password, int n)
+        {
+		int numberOfDigits = 0;
+		for (int i = 0; i < password.length(); i++)
+                {
+			if (Character.isDigit(password.charAt(i)))
+                        {
+				numberOfDigits++;
+			}
+			if (numberOfDigits >= n)
+                        {
+				return true;
+			}
+		}
+		return false;
+    }
+
 }
--- src/Chapter6/P6.java
+++ src/Chapter6/P6.java
@@ -1,14 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Program applies conversion rates to money
+ * @author Geoffrey Fuller
  */
 package Chapter6;
 
+import java.util.Scanner;
+
 /**
- *
- * @author gf0911757
- */
+* Main Method
+*
+* @param args arguments from command line prompt
+*/
 public class P6 {
-    
+     public static void main(String args[]) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many Euros will a Dollar buy? ");
+        double euroDollar = input.nextDouble();
+        System.out.print("How many Pound Sterling will a Dollar buy? ");
+        double poundDollar = input.nextDouble();
+        System.out.print("How many Yen will a Dollar buy? ");
+        double yenDollar = input.nextDouble();
+        
+        String answer = "";
+        while (!answer.equalsIgnoreCase("no")) 
+        {
+            System.out.print("How many dollars are you converting? ");
+            double dollars = input.nextDouble();
+            
+            System.out.println("Enter E for euros, P for pounds, and Y for yen: ");
+            String currency = input.next();
+            switch (currency) 
+            {
+                case "E":                   
+                    System.out.printf("You bought %.2f Euros\n", exchangeMethod(dollars, euroDollar));
+                    break;
+
+                case "P":
+                    System.out.printf("You bought %.2f Pounds\n", exchangeMethod(dollars, poundDollar));
+                    break;
+
+                case "Y":
+                    System.out.printf("You bought %.2f Yen\n", exchangeMethod(dollars, yenDollar));
+                    break;
+            }
+            do 
+            {
+                System.out.print("Would you like to convert again? ");
+                answer = input.next();
+            } while (!answer.equalsIgnoreCase("no") && !answer.equalsIgnoreCase("yes"));
+        }
+
+    }
+
+    public static double exchangeMethod(double dollars, double exchangeRate) 
+    {
+        if (dollars <= 100) 
+        {
+            return (0.90 * dollars * exchangeRate);
+        } else 
+        {
+            return (0.95 * dollars * exchangeRate);
+        }
+
+        
+    }
 }
